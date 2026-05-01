87c2c5fe1a54309da4c18c10818cb6ff96174220
AlsonBasnet07 <basnetalson121@gmail.com>
2026-05-01 12:10:36 +0545

    feat: add Jupyter notebook for creating various NumPy arrays

---
+++ numpy/creatingarray.py
@@ -0,0 +1,7 @@
+import numpy as np
+np.zeros((3, 3))    # 3x3 array of zeros
+np.ones((2, 4))     # 2x4 array of ones
+np.full((2, 2), 7)  # 2x2 array filled with 7
+np.eye(4)           # 4x4 identity matrix
+np.arange(1, 10, 2) # [1, 3, 5, 7, 9] (like range)
+np.linspace(0, 1, 5) # [0. 0.25 0.5 0.75 1.] (evenly spaced)
